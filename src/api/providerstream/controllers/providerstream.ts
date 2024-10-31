54b97ead3c77aca430c9cf13589e0b376bbc254f
Bookflixtv <viubooks.com@gmail.com>
2024-10-31 07:07:46 +0300

    coco

---
+++ src/api/providerstream/controllers/providerstream.ts
@@ -0,0 +1,7 @@
+/**
+ * providerstream controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::providerstream.providerstream');
---
+++ src/api/providerstream/services/providerstream.ts
@@ -0,0 +1,7 @@
+/**
+ * providerstream service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::providerstream.providerstream');
